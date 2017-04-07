6b4e4b5a71826f7c649034a31a5d44463177d2b0
Leonard Lin <leonard.lin@ringcentral.com>
2017-04-07 13:14:43 +0800

    init

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export HUBOT_LOG_LEVEL='debug'
+export HUBOT_GLIP_APP_KEY='Or4e3uZzTBOyiOjrKeH71w'
+export HUBOT_GLIP_APP_SECRET='v1VahfLFRjGQQYj2oUMLzw-5oUWk3GQriYJpsDh2ZcIg'
+export HUBOT_GLIP_USERNAME='+19045591615'
+export HUBOT_GLIP_EXTENSION='101'
+export HUBOT_GLIP_PASSWORD='!QAZ2wsx'
+export HUBOT_GLIP_SERVER='https://platform.devtest.ringcentral.com'
---
+++ scripts/helloWorld.coffee
@@ -0,0 +1,6 @@
+module.exports = (robot) ->
+  robot.hear /hi/i, (res) ->
+    res.reply 'hello world --- 1!'
+    res.send 'send --- 1!'
+  robot.respond /yo/i, (res) ->
+    res.send 'hello world --- 2!'
